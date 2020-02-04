41fe066c641a3e12407f8a42cbd52a1936be22c7
Bu Sun Kim <busunkim@google.com>
2020-02-04 09:31:14 -0800

    chore: add split repo templates

--- noxfile.py
+++ noxfile.py
@@ -21,7 +21,6 @@ import shutil
 import nox
 
 
-LOCAL_DEPS = (os.path.join("..", "api_core"), os.path.join("..", "core"))
 
 @nox.session(python="3.7")
 def lint(session):
@@ -30,7 +29,7 @@ def lint(session):
     Returns a failure if the linters find linting errors or sufficiently
     serious code quality issues.
     """
-    session.install("flake8", "black", *LOCAL_DEPS)
+    session.install("flake8", "black")
     session.run(
         "black",
         "--check",
@@ -70,8 +69,6 @@ def lint_setup_py(session):
 def default(session):
     # Install all test dependencies, then install this package in-place.
     session.install("mock", "pytest", "pytest-cov")
-    for local_dep in LOCAL_DEPS:
-        session.install("-e", local_dep)
     session.install("-e", ".")
 
     # Run py.test against the unit tests.
@@ -89,7 +86,7 @@ def default(session):
     )
 
 
-@nox.session(python=["2.7", "3.5", "3.6", "3.7"])
+@nox.session(python=["2.7", "3.5", "3.6", "3.7", "3.8"])
 def unit(session):
     """Run the unit test suite."""
     default(session)
@@ -116,9 +113,6 @@ def system(session):
     # Install all test dependencies, then install this package into the
     # virtualenv's dist-packages.
     session.install("mock", "pytest")
-    for local_dep in LOCAL_DEPS:
-        session.install("-e", local_dep)
-    session.install("-e", "../test_utils/")
     session.install("-e", ".")
 
     # Run py.test against the system tests.
@@ -157,7 +151,6 @@ def snippets(session):
 
     # Install all test dependencies, then install local packages in place.
     session.install('mock', 'pytest')
-    session.install('-e', '../test_utils/')
     session.install('-e', '.')
     session.run(
         'py.test',
