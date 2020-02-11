d3aa58012b561161ec0d365f80db5fd0d247d40c
Bu Sun Kim <busunkim@google.com>
2020-02-11 15:28:57 -0800

    docs(snippets): use correct helper method

--- docs/snippets_orgs.py
+++ docs/snippets_orgs.py
@@ -32,7 +32,7 @@ def test_get_settings(organization_id):
     client = securitycenter.SecurityCenterClient()
     # organization_id is numeric ID for the organization. e.g.
     # organization_id = "111112223333"
-    org_settings_name = client.organization_path(organization_id)
+    org_settings_name = client.organization_settings_path(organization_id)
 
     org_settings = client.get_organization_settings(org_settings_name)
     print(org_settings)
