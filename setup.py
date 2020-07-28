fb9d45f3dc30609a5df5dad6dfff32d4e4b49007
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2020-07-28 15:00:03 -0700

    Update setup.py
    Co-authored-by: arithmetic1728 <58957152+arithmetic1728@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "Topic :: Internet",
     ],
