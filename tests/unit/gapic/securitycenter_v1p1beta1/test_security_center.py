a91158334051fcf8ed8782a8f2103606912ea43a
yoshi-automation <yoshi-automation@google.com>
2021-01-06 08:17:04 -0800

    chore: upgrade gapic-generator-python to 0.39.1
    feat: add 'from_service_account_info' factory to clients
    fix: fix sphinx identifiers
    PiperOrigin-RevId: 350246057
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Jan 5 16:44:11 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 520682435235d9c503983a360a2090025aa47cd1
    Source-Link: https://github.com/googleapis/googleapis/commit/520682435235d9c503983a360a2090025aa47cd1

--- tests/unit/gapic/securitycenter_v1p1beta1/test_security_center.py
+++ tests/unit/gapic/securitycenter_v1p1beta1/test_security_center.py
@@ -117,8 +117,21 @@ def test__get_default_mtls_endpoint():
     )
 
 
+def test_security_center_client_from_service_account_info():
+    creds = credentials.AnonymousCredentials()
+    with mock.patch.object(
+        service_account.Credentials, "from_service_account_info"
+    ) as factory:
+        factory.return_value = creds
+        info = {"valid": True}
+        client = SecurityCenterClient.from_service_account_info(info)
+        assert client.transport._credentials == creds
+
+        assert client.transport._host == "securitycenter.googleapis.com:443"
+
+
 @pytest.mark.parametrize(
-    "client_class", [SecurityCenterClient, SecurityCenterAsyncClient]
+    "client_class", [SecurityCenterClient, SecurityCenterAsyncClient,]
 )
 def test_security_center_client_from_service_account_file(client_class):
     creds = credentials.AnonymousCredentials()
@@ -137,7 +150,10 @@ def test_security_center_client_from_service_account_file(client_class):
 
 def test_security_center_client_get_transport_class():
     transport = SecurityCenterClient.get_transport_class()
-    assert transport == transports.SecurityCenterGrpcTransport
+    available_transports = [
+        transports.SecurityCenterGrpcTransport,
+    ]
+    assert transport in available_transports
 
     transport = SecurityCenterClient.get_transport_class("grpc")
     assert transport == transports.SecurityCenterGrpcTransport
@@ -6737,7 +6753,7 @@ def test_security_center_host_with_port():
 
 
 def test_security_center_grpc_transport_channel():
-    channel = grpc.insecure_channel("http://localhost/")
+    channel = grpc.secure_channel("http://localhost/", grpc.local_channel_credentials())
 
     # Check that channel is used if provided.
     transport = transports.SecurityCenterGrpcTransport(
@@ -6749,7 +6765,7 @@ def test_security_center_grpc_transport_channel():
 
 
 def test_security_center_grpc_asyncio_transport_channel():
-    channel = aio.insecure_channel("http://localhost/")
+    channel = aio.secure_channel("http://localhost/", grpc.local_channel_credentials())
 
     # Check that channel is used if provided.
     transport = transports.SecurityCenterGrpcAsyncIOTransport(
@@ -6774,7 +6790,7 @@ def test_security_center_transport_channel_mtls_with_client_cert_source(
         "grpc.ssl_channel_credentials", autospec=True
     ) as grpc_ssl_channel_cred:
         with mock.patch.object(
-            transport_class, "create_channel", autospec=True
+            transport_class, "create_channel"
         ) as grpc_create_channel:
             mock_ssl_cred = mock.Mock()
             grpc_ssl_channel_cred.return_value = mock_ssl_cred
@@ -6827,7 +6843,7 @@ def test_security_center_transport_channel_mtls_with_adc(transport_class):
         ssl_credentials=mock.PropertyMock(return_value=mock_ssl_cred),
     ):
         with mock.patch.object(
-            transport_class, "create_channel", autospec=True
+            transport_class, "create_channel"
         ) as grpc_create_channel:
             mock_grpc_channel = mock.Mock()
             grpc_create_channel.return_value = mock_grpc_channel
