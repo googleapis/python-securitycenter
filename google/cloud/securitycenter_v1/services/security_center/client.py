e013d26eadf19e2cee1e4e96d390366cf3db3f73
yoshi-automation <yoshi-automation@google.com>
2021-03-23 16:54:17 -0700

    chore: update gapic-generator-python
    PiperOrigin-RevId: 355923884
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri Feb 5 14:04:52 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 5e3dacee19405529b841b53797df799c2383536c
    Source-Link: https://github.com/googleapis/googleapis/commit/5e3dacee19405529b841b53797df799c2383536c

--- google/cloud/securitycenter_v1/services/security_center/client.py
+++ google/cloud/securitycenter_v1/services/security_center/client.py
@@ -938,6 +938,9 @@ class SecurityCenterClient(metaclass=SecurityCenterClientMeta):
         elif not request:
             request = iam_policy.GetIamPolicyRequest(resource=resource,)
 
+            if resource is not None:
+                request.resource = resource
+
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
         rpc = self._transport._wrapped_methods[self._transport.get_iam_policy]
@@ -1966,6 +1969,9 @@ class SecurityCenterClient(metaclass=SecurityCenterClientMeta):
         elif not request:
             request = iam_policy.SetIamPolicyRequest(resource=resource,)
 
+            if resource is not None:
+                request.resource = resource
+
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
         rpc = self._transport._wrapped_methods[self._transport.set_iam_policy]
@@ -2047,6 +2053,12 @@ class SecurityCenterClient(metaclass=SecurityCenterClientMeta):
             request = iam_policy.TestIamPermissionsRequest(
                 resource=resource, permissions=permissions,
             )
+
+            if resource is not None:
+                request.resource = resource
+
+            if permissions:
+                request.permissions.extend(permissions)
 
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
