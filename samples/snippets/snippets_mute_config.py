c9d894e0f2679b3c0212cb74e03159fc48d75883
SitaLakshmi <sita1996@gmail.com>
2022-02-03 02:06:49 +0530

    docs(samples): applied documentation review comments

--- samples/snippets/snippets_mute_config.py
+++ samples/snippets/snippets_mute_config.py
@@ -22,13 +22,13 @@ from google.cloud.securitycenter_v1 import Finding
 def create_mute_rule(parent_path: str, mute_config_id: str) -> None:
     """
     Creates a mute configuration under a given scope that will mute
-    all new finding which match the filter/ mute rule.
+    all new findings that match a given filter.
     Existing findings will NOT BE muted.
     Args:
-        parent_path: use any one of the following three options,
-                    organizations/{organization} or
-                    folders/{folder} or
-                    projects/{project}
+        parent_path: use any one of the following options:
+                     - organizations/{organization_id}
+                     - folders/{folder_id}
+                     - projects/{project_id}
         mute_config_id: Set a unique id; max of 63 chars.
     """
 
@@ -40,7 +40,7 @@ def create_mute_rule(parent_path: str, mute_config_id: str) -> None:
     mute_config.description = "Mute low-medium IAM grants excluding 'compute' "
     # Set mute rule(s).
     # To construct mute rules and for supported properties, see:
-    # https://cloud.google.com/security-command-center/docs/how-to-mute-findings#console_3
+    # https://cloud.google.com/security-command-center/docs/how-to-mute-findings#create_mute_rules
     mute_config.filter = (
         'severity="LOW" OR severity="MEDIUM" AND '
         'category="Persistence: IAM Anomalous Grant" AND '
@@ -53,7 +53,7 @@ def create_mute_rule(parent_path: str, mute_config_id: str) -> None:
     request.mute_config = mute_config
 
     mute_config = client.create_mute_config(request=request)
-    print(f"Mute Config Rule created successfully: {mute_config.name}")
+    print(f"Mute rule created successfully: {mute_config.name}")
 
 
 # [END securitycenter_create_mute_config]
@@ -63,13 +63,13 @@ def create_mute_rule(parent_path: str, mute_config_id: str) -> None:
 def delete_mute_rule(mute_config_name: str) -> None:
     """
     Deletes a mute configuration given its resource name.
-    Note: Already muted findings are not affected when a mute config is deleted.
+    Note: Previously muted findings are not affected when a mute config is deleted.
     Args:
         mute_config_name: Specify the name of the mute config to delete.
-        Its format is:
-        organizations/{organization}/muteConfigs/{config_id} or
-        folders/{folder}/muteConfigs/{config_id} or
-        projects/{project}/muteConfigs/{config_id}
+                          Use any one of the following formats:
+                          - organizations/{organization}/muteConfigs/{config_id}
+                          - folders/{folder}/muteConfigs/{config_id} or
+                          - projects/{project}/muteConfigs/{config_id}
     """
     from google.cloud import securitycenter
 
@@ -79,7 +79,7 @@ def delete_mute_rule(mute_config_name: str) -> None:
     request.name = mute_config_name
 
     client.delete_mute_config(request)
-    print(f"Mute Config Rule deleted successfully: {mute_config_name}")
+    print(f"Mute rule deleted successfully: {mute_config_name}")
 
 
 # [END securitycenter_delete_mute_config]
@@ -88,13 +88,13 @@ def delete_mute_rule(mute_config_name: str) -> None:
 # [START securitycenter_get_mute_config]
 def get_mute_rule(mute_config_name: str) -> None:
     """
-    Retrieves mute configuration given its resource name.
+    Retrieves a mute configuration given its resource name.
     Args:
         mute_config_name: Name of the mute config to retrieve.
-        Its format is:
-        organizations/{organization}/muteConfigs/{config_id} or
-        folders/{folder}/muteConfigs/{config_id} or
-        projects/{project}/muteConfigs/{config_id}
+                          Use any one of the following formats:
+                          - organizations/{organization}/muteConfigs/{config_id}
+                          - folders/{folder}/muteConfigs/{config_id}
+                          - projects/{project}/muteConfigs/{config_id}
     """
     from google.cloud import securitycenter
 
@@ -104,7 +104,7 @@ def get_mute_rule(mute_config_name: str) -> None:
     request.name = mute_config_name
 
     mute_config = client.get_mute_config(request)
-    print(f"Retrieved the mute config: {mute_config}")
+    print(f"Retrieved the mute rule: {mute_config}")
 
 
 # [END securitycenter_get_mute_config]
@@ -113,15 +113,15 @@ def get_mute_rule(mute_config_name: str) -> None:
 # [START securitycenter_list_mute_configs]
 def list_mute_rules(parent: str) -> None:
     """
-    Listing mute configs at organization level will give all the configs
-    at org, folder and project levels.
+    Listing mute configs at organization level will return all the configs
+    at the org, folder and project levels.
     Similarly, listing configs at folder level will list all the configs
-    at folder + projects (within that folder) level.
+    at the folder and project levels.
     Args:
-        parent: Use any one of the following three resource paths to list mute configurations.
-        "organizations/{organization_id}" or
-        "folders/{folder_id}" or
-        "projects/{project_id}"
+        parent: Use any one of the following resource paths to list mute configurations:
+                - organizations/{organization_id}
+                - folders/{folder_id}
+                - projects/{project_id}
     """
     from google.cloud import securitycenter
 
@@ -145,10 +145,10 @@ def update_mute_rule(mute_config_name: str) -> None:
     The following can be updated in a mute config: description, and filter/ mute rule.
     Args:
         mute_config_name: Specify the name of the mute config to delete.
-        Its format is:
-        organizations/{organization}/muteConfigs/{config_id} or
-        folders/{folder}/muteConfigs/{config_id} or
-        projects/{project}/muteConfigs/{config_id}
+                          Use any one of the following formats:
+                          - organizations/{organization}/muteConfigs/{config_id}
+                          - folders/{folder}/muteConfigs/{config_id}
+                          - projects/{project}/muteConfigs/{config_id}
     """
     from google.cloud import securitycenter
     from google.protobuf import field_mask_pb2
@@ -164,14 +164,14 @@ def update_mute_rule(mute_config_name: str) -> None:
     request = securitycenter.UpdateMuteConfigRequest()
     request.mute_config = update_mute_config
     # Set the update mask to specify which properties of the Mute Config should be updated.
-    # If empty all mutable fields will be updated.
+    # If empty, all mutable fields will be updated.
     # Make sure that the mask fields match the properties changed in 'update_mute_config'.
     # For more info on constructing update mask path, see the proto or:
     # https://cloud.google.com/security-command-center/docs/reference/rest/v1/folders.muteConfigs/patch?hl=en#query-parameters
     request.update_mask = field_mask
 
     mute_config = client.update_mute_config(request)
-    print(f"Updated mute config : {mute_config}")
+    print(f"Updated mute rule : {mute_config}")
 
 
 # [END securitycenter_update_mute_config]
@@ -181,15 +181,16 @@ def update_mute_rule(mute_config_name: str) -> None:
 def set_mute_unmute_finding(finding_path: str) -> None:
     """
       Mute/unmute an individual finding.
-      If a finding is already muted, muting it again (or vice versa) is a no-op.
-      Various mute states are: MUTE_UNSPECIFIED/ MUTE/ UNMUTE.
+      If a finding is already muted, muting it again has no effect.
+      Similarly, unmuting a finding that isn't muted has no effect.
+      Various mute states are: MUTE_UNSPECIFIED/MUTE/UNMUTE.
     Args:
         finding_path: The relative resource name of the finding. See:
         https://cloud.google.com/apis/design/resource_names#relative_resource_name
-        Example:
-        "organizations/{organization_id}/sources/{source_id}/finding/{finding_id}",
-        "folders/{folder_id}/sources/{source_id}/finding/{finding_id}",
-        "projects/{project_id}/sources/{source_id}/finding/{finding_id}".
+        Use any one of the following formats:
+        - organizations/{organization_id}/sources/{source_id}/finding/{finding_id},
+        - folders/{folder_id}/sources/{source_id}/finding/{finding_id},
+        - projects/{project_id}/sources/{source_id}/finding/{finding_id}.
     """
     from google.cloud import securitycenter
 
@@ -209,14 +210,14 @@ def set_mute_unmute_finding(finding_path: str) -> None:
 # [START securitycenter_bulk_mute]
 def bulk_mute_findings(parent_path: str, mute_rule: str) -> None:
     """
-      Kicks off an LRO (Long-Running-Operation) to bulk mute findings for a parent based on a filter.
-      The parent can be either an organization, folder or project. The findings
+      Kicks off a long-running operation (LRO) to bulk mute findings for a parent based on a filter.
+      The parent can be either an organization, folder, or project. The findings
       matched by the filter will be muted after the LRO is done.
     Args:
-        parent_path: use any one of the following three options
-            organizations/{organization} or
-            folders/{folder} or
-            projects/{project}
+        parent_path: use any one of the following options:
+                     - organizations/{organization}
+                     - folders/{folder}
+                     - projects/{project}
         mute_rule: Expression that identifies findings that should be updated.
     """
     from google.cloud import securitycenter
@@ -230,7 +231,7 @@ def bulk_mute_findings(parent_path: str, mute_rule: str) -> None:
     request.filter = mute_rule
 
     response = client.bulk_mute_findings(request)
-    print(f"Bulk mute findings completed successfully ! : {response}")
+    print(f"Bulk mute findings completed successfully! : {response}")
 
 
 # [END securitycenter_bulk_mute]
